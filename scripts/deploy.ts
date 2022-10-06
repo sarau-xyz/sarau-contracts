8d85ddf67f28103e27fbfe9872a0a812a3ed328c
Gustavo <gustavo@obex.pw>
2022-10-06 09:30:23 -0300

    finished deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,8 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
+  const bytes32Currency = ethers.utils.formatBytes32String("CELO");
+
   const SarauNFT = await ethers.getContractFactory("SarauNFT");
   const _sarauNFTInstance = await SarauNFT.deploy();
 
@@ -9,12 +11,23 @@ async function main() {
   const SarauMaker = await ethers.getContractFactory("SarauMaker");
   const _sarauMakerInstance = await SarauMaker.deploy(
     _sarauNFTInstance.address,
-    ethers.utils.formatBytes32String('CELO'),
+    bytes32Currency
   );
 
   await _sarauMakerInstance.deployed();
 
   console.log(`SarauMaker deployed to ${_sarauMakerInstance.address}`);
+
+  // verify SarauMaker
+  await run("verify:verify", {
+    address: _sarauMakerInstance.address,
+    constructorArguments: [_sarauNFTInstance.address, bytes32Currency],
+  });
+
+  // verify SarauNFT
+  await run("verify:verify", {
+    address: _sarauNFTInstance.address,
+  });
 }
 
 main().catch((error) => {
